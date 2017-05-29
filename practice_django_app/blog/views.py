8f257ffdadb1c7e51d6a6be015b99f93ac074d20
hongdonghyun <hong4367@gmail.com>
2017-05-29 19:32:05 +0900

    add html,view,url,stream

--- practice_django_app/blog/views.py
+++ practice_django_app/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+
+    context = {
+        'title' :'PostList from Post_list view',
+        'post' : posts
+    }
+
+    return render(request,'blog/post_list.html',context=context)
